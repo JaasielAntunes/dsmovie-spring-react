503cc9423720714619b765fa444d84f7d86d1a7b
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2022-01-11 10:56:35 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
-import NavBar from './components/NavBar';
+import Navbar from 'components/Navbar';
+import Form from 'pages/Form';
+import Listing from 'pages/Listing';
+import {
+  BrowserRouter, Route, Routes
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
